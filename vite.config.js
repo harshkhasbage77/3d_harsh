059cbdc26272e98986177efafca01d1728b4edfb
harshkhasbage77 <harshkhasbage77@gmail.com>
2024-11-03 23:03:16 +0530

    Vite deployment documentation followed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3d_harsh/',
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
